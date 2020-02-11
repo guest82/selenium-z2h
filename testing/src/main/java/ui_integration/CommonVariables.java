022af8a9451ba83a038cc446e4a2dc803eac89fb
mayzee <Evgeny.Mayzel@emc.com>
2020-02-11 14:32:05 +0200

    add account and pwd

--- testing/src/main/java/ui_integration/CommonVariables.java
+++ testing/src/main/java/ui_integration/CommonVariables.java
@@ -3,8 +3,8 @@ package ui_integration;
 public class CommonVariables {
 
     public static String BASE_URL = "https://github.com/login";
-    public static String USERNAME = "jeniamayzel";
-    public static String PASSWORD = "";
+    public static String USERNAME = "jeniamayzelTest";
+    public static String PASSWORD = "Welcome1!Kashya123!";
     public static final String WEBSITE_TITLE = "GitHub";
 
 }
